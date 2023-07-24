5f5bf28b9aa300c5a238fe1b3b793103009eea4d
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-07-24 16:45:18 +0400

    Added react router and configuration

--- frontend/inventory-system/src/main.jsx
+++ frontend/inventory-system/src/main.jsx
@@ -1,10 +1,29 @@
+
+//!React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './css/dashboard.css'
 
+
+//!React Router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+//!Custom Components
+import OrderComponent from '../routes/Order.jsx'
+import ProductCreateComponent from '../routes/ProductCreate.jsx'
+
+
+
+//?Render component to client site dynamically
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<App/>}/>
+          <Route path="/create" element={<ProductCreateComponent/>}/>
+          <Route path="/order" element={<OrderComponent/>}/>
+        </Routes>
+    </BrowserRouter>
 )
