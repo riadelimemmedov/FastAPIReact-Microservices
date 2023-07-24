8d9ec728f11894900935bf0180359061226ddef9
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-07-24 16:43:17 +0400

    Updated App component

--- frontend/inventory-system/src/App.jsx
+++ frontend/inventory-system/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
+
+//!React
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+
+//!Custom Components
+import HeaderComponent from '../components/Header.jsx'
+import SidebarComponent from '../components/Sidebar.jsx'
+import BodyComponent from '../components/Body.jsx'
+
+
+
+//!Third Party Package
+import { Toaster,toast } from "react-hot-toast";
+
+
+//*App
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container-fluid">
+        <div className="row">
+
+          {/* Header */}
+          <HeaderComponent/>
+
+
+          {/* Sidebar */}
+          <SidebarComponent/>
+
+
+          {/* Body */}
+          <BodyComponent/>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toaster/>
     </>
   )
 }
-
 export default App
