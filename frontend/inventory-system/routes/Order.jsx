81531a77533d0bf28521c6e24effd2e31522807b
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-07-24 16:37:52 +0400

    Created Order component for order process

---
+++ frontend/inventory-system/routes/Order.jsx
@@ -0,0 +1,17 @@
+
+//!React
+import { useState,useEffect } from 'react'
+
+
+
+//*BodyComponent
+const OrderComponent = () => {
+    return(
+        <>
+            <div>
+                Hello From Order 
+            </div>
+        </>
+    )
+}
+export default OrderComponent;
