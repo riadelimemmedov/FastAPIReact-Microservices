d709ef0e8499ba332cc48dc0f900df845479d95f
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-07-24 16:35:57 +0400

    Created Sidebar component

---
+++ frontend/inventory-system/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+//!React
+import { useState,useEffect } from 'react'
+
+
+
+//*SidebarComponent
+const SidebarComponent = () => {
+    return(
+        <>  
+            <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div className="position-sticky pt-3 sidebar-sticky">    
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <a className="nav-link text-black fw-bold h2" aria-current="page" href="#">
+                                Products
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+export default SidebarComponent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
