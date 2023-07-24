4e20aef098235707d300ade332db033835ef5906
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-07-24 16:35:27 +0400

    Created Header component

---
+++ frontend/inventory-system/components/Header.jsx
@@ -0,0 +1,29 @@
+
+//!React
+import { useState,useEffect } from 'react'
+
+
+//!Third Party Package
+import { Toaster } from 'react-hot-toast';
+
+
+
+//*Header
+const HeaderComponent = () => {
+    return (
+        <>  
+            <Toaster/>
+            <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/">WareHouse Microservices</a>
+                <div className="navbar-nav">
+                    <div className="nav-item text-nowrap">
+                        <a className="nav-link px-3" href="#">Sign out</a>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+export default HeaderComponent
+
+
