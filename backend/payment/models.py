fc1b6cefb004b2a6fd2e0f053d5e8882f16dbaaf
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-07-31 18:38:55 +0400

    Added customer_id row to order table

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -33,7 +33,8 @@ class Order(JsonModel):
     product_id:str = Field(index=True,full_text_search=True)
     product_name:str = Field(default="product_name",full_text_search=True)
     slug:str = Field(default=f"order")
-    price:float
+    customer_id:str = Field(index=True,full_text_search=True)
+    price:float 
     fee:float
     total:float
     quantity:int
