b96fea5f42a4a1753215219f091166f4043c3bd4
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-08-09 16:51:17 +0400

    Adding custom style for  material ui componenet

--- frontend/inventory-system/style/style.js
+++ frontend/inventory-system/style/style.js
@@ -1,7 +1,8 @@
+//!MateriUI
 import { makeStyles } from '@material-ui/core/styles'
 
 
-//!useStyles
+//*useStyles
 const useStyles = makeStyles((theme) =>({
     cardGrid:{
         paddingTop:'20px 0px'
